08f5c07db009e764259d69c37ce4c59a9ec75fe1
Hafiffdl <hafif.fdl16@gmail.com>
2024-08-11 13:40:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex justify-center bg-cyan-500 min-h-screen items-center">
+      <div>
+        hello world
+      </div>
+    </div>
+  )
+}
+
+export default App
