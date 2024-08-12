025bcad093d7742c460686d70c106641bf17740c
Hafiffdl <hafif.fdl16@gmail.com>
2024-08-12 17:18:02 +0700

    edit Input jsx

--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -7,6 +7,7 @@ const Input = (props) => {
             className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"
             placeholder={placeholder}
             name={name}
+            id={name}
         />
     )
 }
